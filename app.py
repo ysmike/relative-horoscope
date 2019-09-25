adbd45422338dd944eae625e161922eea12c49fe
Mike Lee <xcalibur220@gmail.com>
2019-09-25 15:24:55 -0400

    Assign PORT variable for deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import os
 from pprint import pprint
 import requests
 
@@ -139,4 +140,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
